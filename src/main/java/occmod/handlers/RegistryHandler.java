6dbc2ac3540e90a2543725c809d6fb487fc667d3
git4j2 <git4j2@users.noreply.github.com>
2019-06-10 10:59:05 -0400

    v3.0 - Tile, Block and Axe.

--- src/main/java/occmod/handlers/RegistryHandler.java
+++ src/main/java/occmod/handlers/RegistryHandler.java
@@ -1,15 +1,26 @@
 package occmod.handlers;
 
+import occmod.init.BlockInit;
 import occmod.init.ItemInit;
+import occmod.init.ToolInit;
 
 public class RegistryHandler {
     
     public static void Client() {
     	ItemInit.register();
+    	ToolInit.register();
+    	BlockInit.registerRenders();
+    	
     }
     
     public static void Common() {
     	ItemInit.init();
+    	BlockInit.init();
+    	ToolInit.init();
+    	BlockInit.register();
     	ItemInit.Recipitate();
+    	BlockInit.Recipitate();
+    	ToolInit.Recipitate();
+    	
     }
 }
